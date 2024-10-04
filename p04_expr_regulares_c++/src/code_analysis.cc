9e5016783e13965050ac87b10c0a30fe47dbe48b
usuario <alu0101539157@ull.edu.es>
2024-10-04 21:08:21 +0000

    regex var loops comm main, ifs deteccion regex en proceso

--- p04_expr_regulares_c++/src/code_analysis.cc
+++ p04_expr_regulares_c++/src/code_analysis.cc
@@ -28,9 +28,35 @@ void CodeAnalysis::LineParser(const std::string& line, int line_counter) {
   std::smatch matches;
 
   // regex a analizar
-  std::regex inline_comments_regex("\/\/(.*)");
-  std::regex multiline_comments_regex("((\/\*\*)(((\n\s\*).*)+))(\n\s\*\/)");
-  std::regex variables_regex();
-  std::regex loops_regex();
+  std::regex inline_comments_regex("\/\/(.*)"); // podrá ser + y no tener un comentario vacio ?
 
+  static bool is_multiline_comment = false;
+  std::regex ini_multiline_comments_regex(R"(\/\*(.*))");
+  std::regex end_multiline_comments_regex(R"(^\*\/$)");
+
+  std::regex int_variables_regex(R"(^\s*int\s\w+(;|\{\d+\};|\s=\s\d+;|\{\};))");
+  std::regex double_variables_regex(R"(^\s*double\s\w+(;|\{\d+\.\d+\};|\{\d+\};|\s=\s\d+;|\s=\s\d+\.\d+;)");
+
+  std::regex for_loops_regex(R"(^\bfor\s\(.*\)\s\{$)");
+  std::regex while_loops_regex(R"(^\bwhile\s\(.*\)\s\{$)");
+
+  std::regex has_main(R"(^int\smain\(.*\)\s\{?)");
+
+  // detectar comentarios inline
+  if (std::regex_search(line, matches, inline_comments_regex)) {
+    Comment comment(line_counter, line_counter, matches[1].str(), "inline");  // creación de objeto Comentario
+    comments_.pushback(comment);  // pushback a vector de comentarios de CodeBlock
+    return;
+  }
+
+  // detectar comentarios multilínea
+  if(is_multiline_comment) {
+    if (std::regex_search(line, matches, end_multiline_comments_regex)) {
+      Comment comment()
+    }
+  }
+
+  
 }
+
+//loops
